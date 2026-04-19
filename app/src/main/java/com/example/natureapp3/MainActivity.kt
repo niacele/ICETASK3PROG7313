bb9f086a61651948657554b193490f356df319e7
Nia Cele <st10439898@vcconnect.edu.za>
2026-04-19 23:38:17 +0200

    Added the UI elements

--- app/src/main/java/com/example/natureapp3/MainActivity.kt
+++ app/src/main/java/com/example/natureapp3/MainActivity.kt
@@ -5,8 +5,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
